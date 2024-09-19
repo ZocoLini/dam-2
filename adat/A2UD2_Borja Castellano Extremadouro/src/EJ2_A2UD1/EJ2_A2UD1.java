3e124beb4ebd91ca347106af6e84e7544468ccf8
Borja Castellano <borjacastellano1@gmail.com>
2024-09-19 11:16:18 +0200

    (adat) Primeros tres ejercicios e A2UD1

---
+++ adat/A2UD2_Borja Castellano Extremadouro/src/EJ2_A2UD1/EJ2_A2UD1.java
@@ -0,0 +1,58 @@
+package EJ2_A2UD1;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+public class EJ2_A2UD1
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Introduce la ruta a un directorio");
+        String ruta = System.console().readLine();
+
+        File directorio = new File(ruta);
+
+        if (!directorio.exists())
+        {
+            System.out.println("El directorio no existe");
+            return;
+        }
+
+        if (!directorio.isDirectory())
+        {
+            System.out.println("La ruta no corresponde a un directorio");
+            return;
+        }
+
+        System.out.println("--- LISTANDO EL DIRECTORIO " + directorio.getAbsolutePath() + " ---");
+
+        for (File variable : directorio.listFiles())
+        {
+            if (variable.isFile())
+            {
+                printFile(variable);
+            }
+            else
+            {
+                printDirectory(variable);
+            }
+        }
+    }
+
+    public static void printFile(File file)
+    {
+        System.out.println("-| " + file.getName() + " <FICHERO> "
+                + file.length() / 1024.0 + " Kbytes " +
+                LocalDateTime.ofEpochSecond(
+                        file.lastModified() / 1000,
+                        0,
+                        ZoneOffset.UTC).toString().replace('T', ' ')
+        );
+    }
+
+    public static void printDirectory(File directory)
+    {
+        System.out.println("-| " + directory.getName() + " <DIR>");
+    }
+}
