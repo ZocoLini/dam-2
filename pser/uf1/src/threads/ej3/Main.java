372b4b34855ccdf38b81f9e6dd6373f8bc4d962d
Borja Castellano <borjacastellano1@gmail.com>
2024-10-08 14:26:17 +0200

    (pser) viva los hilos x2

--- pser/uf1/src/threads/ej3/Main.java
+++ pser/uf1/src/threads/ej3/Main.java
@@ -2,63 +2,71 @@ package threads.ej3;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.atomic.AtomicInteger;
 
 public class Main
 {
     private static final int CENSO = 10000;
-    private static final int NUM_PARTIDOS = 5;
+
+    private static final Partido[] partidos =
+            {
+                    new Partido("PP"),
+                    new Partido("PSOE"),
+                    new Partido("VOX"),
+                    new Partido("SUMAR"),
+            };
 
     public static void main(String[] args)
     {
-        AtomicInteger[] votos = new AtomicInteger[NUM_PARTIDOS];
         List<Votante> votantes = new ArrayList<>();
-
-        for (int i = 0; i < NUM_PARTIDOS; i++)
-        {
-            votos[i] = new AtomicInteger();
-        }
 
         for (int i = 0; i < CENSO; i++)
         {
-            var votante = new Votante(votos);
+            var votante = new Votante(partidos);
             votante.start();
             votantes.add(votante);
         }
 
         try
         {
-            for (var votante : votantes)
-            {
-                votante.join();
-            }
-        } catch (InterruptedException e)
+            for (var votante : votantes) votante.join();
+        }
+        catch (InterruptedException e)
         {
             throw new RuntimeException(e);
         }
 
-        int sum = votos[0].get();
+
+        int sum = partidos[0].getVotos();
         List<Integer> winners = new ArrayList<>();
         winners.add(0);
-        for (int i = 1; i < NUM_PARTIDOS; i++)
+
+        for (int i = 1; i < partidos.length; i++)
         {
-            var value = votos[i].get();
+            var value = partidos[i].getVotos();
+            final var actualWinnerCount = partidos[winners.getFirst()].getVotos();
             sum += value;
 
-            if (value > votos[winners.getFirst()].get())
+            if (value > actualWinnerCount)
             {
                 winners.clear();
                 winners.add(i);
-            } else if (value == winners.getFirst())
+            }
+            else
             {
-                winners.add(i);
+                if (value == actualWinnerCount)
+                {
+                    winners.add(i);
+                }
             }
         }
 
         System.out.println(winners.size() == 1 ? "Ganador: " : "Empate entre: ");
         for (var winner : winners)
         {
-            System.out.println("\tPartido " + winner + " con " + votos[winner].get() + " votos");
+            final var partido = partidos[winner];
+
+            System.out.println("\tEl partido " + partido.getNombre() + " con " + partido.getVotos() + " " +
+                    "partidos");
         }
 
         System.out.println("Votos totales: " + sum);
--- pser/uf1/src/threads/ej3/Votante.java
+++ pser/uf1/src/threads/ej3/Votante.java
@@ -1,12 +1,12 @@
 package threads.ej3;
 
-import java.util.concurrent.atomic.AtomicInteger;
-
 public class Votante extends Thread
 {
-    private final AtomicInteger[] partidos;
+    private static final int TIME_TO_WAIT = 500;
+    
+    private final Partido[] partidos;
 
-    public Votante(AtomicInteger[] partidos)
+    public Votante(Partido[] partidos)
     {
         this.partidos = partidos;
     }
@@ -16,13 +16,13 @@ public class Votante extends Thread
     {
         try
         {
-            Thread.sleep((int) (Math.random() * 500));
+            Thread.sleep((int) (Math.random() * TIME_TO_WAIT));
         } catch (InterruptedException e)
         {
             throw new RuntimeException(e);
         }
 
         int voto = (int) (Math.random() * partidos.length);
-        partidos[voto].incrementAndGet();
+        partidos[voto].increment();
     }
 }
