571eb2cb6511ea7660a887d1a7e1d5284a2f68aa
Borja Castellano <borjacastellano1@gmail.com>
2024-12-10 09:42:37 +0100

    (adat) finalizando tarea 2

--- adat/A2UD3_Borja_Castellano_Extremadouro/src/main/resources/a.sql
+++ adat/A2UD3_Borja_Castellano_Extremadouro/src/main/resources/a.sql
@@ -43,9 +43,35 @@ select Num_proxecto, num_proxecto, nome_proxecto, lugar, num_departamento_contro
 from PROXECTO
 where Num_departamento_controla = (select Num_departamento from DEPARTAMENTO where Nome_departamento = ?)
 
-create proc pr_cambioDomicilio(@nss varchar, @rua varchar, @numero_rua int, @piso varchar, @cp int, @localidade varchar) as
+create proc pr_cambioDomicilio(@nss varchar, @rua varchar, @numero_rua int, @piso varchar, @cp int,
+                               @localidade varchar) as
 begin
     update EMPREGADO
-    set Rua = @rua, Numero_rua = @numero_rua, Piso = @piso, CP = @cp, Localidade = @localidade
+    set Rua        = @rua,
+        Numero_rua = @numero_rua,
+        Piso       = @piso,
+        CP         = @cp,
+        Localidade = @localidade
     where NSS = @nss
+end
+go
+create proc pr_DatosProxecto(@num_proxecto int, @nombre varchar output, @lugar varchar output,
+                             @num_dep int output) as
+begin
+    set @nombre = (select Nome_proxecto from PROXECTO where Num_proxecto = @num_proxecto)
+    set @lugar = (select Lugar from PROXECTO where  Num_proxecto = @num_proxecto)
+    set @num_dep = (select Num_departamento_controla from PROXECTO where  Num_proxecto = @num_proxecto)
+end
+go
+create proc pr_DepartControlaProxe(@num_min_proxec int) as
+begin
+
+end
+go
+create function fn_nEmpDepart(@nome_dep varchar) returns int as
+begin
+    return (select count(*)
+            from EMPREGADO
+            where Num_departamento_pertenece =
+                  (select Num_departamento from DEPARTAMENTO where Nome_departamento = @nome_dep))
 end
