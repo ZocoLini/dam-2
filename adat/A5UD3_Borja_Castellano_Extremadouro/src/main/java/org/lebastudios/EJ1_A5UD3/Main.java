9f79b9f8da0b1e35377696277e6f4c5fc0fabc52
Borja Castellano <borjacastellano1@gmail.com>
2024-12-19 03:02:17 -0500

    (adat) A5UD3

---
+++ adat/A5UD3_Borja_Castellano_Extremadouro/src/main/java/org/lebastudios/EJ1_A5UD3/Main.java
@@ -0,0 +1,42 @@
+package org.lebastudios.EJ1_A5UD3;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Arrays;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        String db = args[0];
+        String user = args[1];
+        String pass = args[2];
+
+        var consulta = Arrays.copyOfRange(args, 3, args.length);
+
+        var consultas = String.join(" ", consulta).split(";");
+
+        try
+        {
+            Connection con = DriverManager.getConnection(
+                    String.format("jdbc:sqlserver://10.0.2.4\\MV-PROG:1433;database=%s;trustServerCertificate=true",
+                            db), user, pass);
+
+            Statement statement = con.createStatement();
+
+            for (String s : consultas)
+            {
+                statement.addBatch(s);
+            }
+            
+            statement.executeBatch();
+            statement.close();
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ adat/A5UD3_Borja_Castellano_Extremadouro/src/main/java/org/lebastudios/EJ2_A5UD3/Main.java
@@ -0,0 +1,9 @@
+package org.lebastudios.EJ2_A5UD3;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+         
+    }
+}
