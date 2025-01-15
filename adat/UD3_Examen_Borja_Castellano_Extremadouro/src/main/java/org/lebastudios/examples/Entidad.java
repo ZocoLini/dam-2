6b939f422cafe789bc8e65bf5e1dfcf373db92bf
Borja Castellano <borjacastellano1@gmail.com>
2025-01-15 13:12:56 +0100

    (adat) preparando el examen

---
+++ adat/UD3_Examen_Borja_Castellano_Extremadouro/src/main/java/org/lebastudios/examples/Entidad.java
@@ -0,0 +1,15 @@
+package org.lebastudios.examples;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Entidad
+{
+    private String nombre;
+}
