6e0ae5d206b9e0d5910ed32c443de536de5a8b05
Borja Castellano <borjacastellano1@gmail.com>
2025-01-15 23:07:35 +0100

    (adat) preparando el examen

---
+++ adat/UD3_Examen_Borja_Castellano_Extremadouro/src/main/java/org/lebastudios/database/Database.java
@@ -0,0 +1,121 @@
+package org.lebastudios.database;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+public class Database
+{
+    private static final String DB_URL = "jdbc:sqlserver://localhost:1433;database=BDEmpresa;trustServerCertificate=true";
+    private static final String DB_USER = "sa";
+    private static final String DB_PASS = "abc123.";
+    
+    private static Database instance;
+    
+    public static Database getInstance()
+    {
+        if (instance == null) instance = new Database();
+        
+        return instance;
+    }
+    
+    private Database() {}
+
+    private Connection getConnection() throws SQLException
+    {
+        return java.sql.DriverManager.getConnection(DB_URL, DB_USER, DB_PASS);
+    }
+    
+    public boolean connectTransaction(Consumer<Connection> consumer)
+    {
+        Connection connection = null;
+
+        try
+        {
+            connection = getConnection();
+            connection.setAutoCommit(false);
+            consumer.accept(connection);
+            connection.commit();
+            return true;
+        }
+        catch (Exception e)
+        {
+            try
+            {
+                if (connection != null) connection.rollback();
+            }
+            catch (Exception ex)
+            {
+                ex.printStackTrace();
+            }
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                if (connection != null) connection.close();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+        
+        return false;
+    }
+    
+    public <T> T connectQuery(Function<Connection, T> query)
+    {
+        Connection connection = null;
+
+        try
+        {
+            connection = getConnection();
+            return query.apply(connection);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+            return null;
+        }
+        finally
+        {
+            try
+            {
+                if (connection != null) connection.close();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void connectQuery(Consumer<Connection> query)
+    {
+        Connection connection = null;
+
+        try
+        {
+            connection = getConnection();
+            query.accept(connection);
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        finally
+        {
+            try
+            {
+                if (connection != null) connection.close();
+            }
+            catch (Exception e)
+            {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- adat/UD3_Examen_Borja_Castellano_Extremadouro/src/main/java/org/lebastudios/sqlx/SQLx.java
+++ adat/UD3_Examen_Borja_Castellano_Extremadouro/src/main/java/org/lebastudios/sqlx/SQLx.java
@@ -1,10 +1,7 @@
 package org.lebastudios.sqlx;
 
 import java.lang.reflect.InvocationTargetException;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
