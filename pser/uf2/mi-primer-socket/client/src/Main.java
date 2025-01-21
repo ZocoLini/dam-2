8eafbe1efed1ee5813efec07f8c05d4bb3f41686
Borja Castellano <borjacastellano1@gmail.com>
2025-01-21 14:21:32 +0100

    (pser) añadido sesiones

--- pser/uf2/mi-primer-socket/client/src/Main.java
+++ pser/uf2/mi-primer-socket/client/src/Main.java
@@ -2,7 +2,6 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Main
@@ -13,22 +12,24 @@ public class Main
         {
             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
-            
+
             String msg = "";
-            
+
             while (!msg.equals("fin"))
             {
                 msg = new Scanner(System.in).nextLine();
+
+                if (socket.isClosed()) 
+                {
+                    System.out.println("Se ha cerrado la conexión");
+                    return;
+                }
                 
                 outputStream.writeUTF(msg);
                 String receivedMsg = inputStream.readUTF();
 
                 System.out.println(receivedMsg);
             }
-        }
-        catch (UnknownHostException e)
-        {
-            throw new RuntimeException(e);
         }
         catch (IOException e)
         {
--- pser/uf2/mi-primer-socket/src/Main.java
+++ pser/uf2/mi-primer-socket/src/Main.java
@@ -1,11 +1,15 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Main
 {
     private static final int PORT = 8080;
     private static boolean SERVER_UP = true;
+    
+    private static final Map<String, Session> sessions = new ConcurrentHashMap<>();
     
     public static void main(String[] args)
     {
@@ -25,28 +29,45 @@ public class Main
     
     private static void talkWithAClient(Socket socket)
     {
-        try
+        try (socket)
         {
             DataInputStream inputStream = new DataInputStream(socket.getInputStream());
             DataOutputStream outputStream = new DataOutputStream(socket.getOutputStream());
 
             boolean exit = false;
 
+            String username = inputStream.readUTF();
+            
+            Session session = sessions.get(username);
+            if (session == null) 
+            {
+                session = new Session(socket, username);
+                sessions.put(username, session);
+            }
+            else
+            {
+                if (session.isConnected()) session.disconnect();
+                
+                session.newConnection(socket);
+            }
+            
+            outputStream.writeUTF("Session iniciada: " + username + " " + session.getLogins() + " veces connectado");
+            
             while (!exit)
             {
                 String input = inputStream.readUTF();
-
+                outputStream.writeUTF(input);
+                
                 if (input.equals("fin"))
                 {
                     exit = true;
+                    session.disconnect();
                 }
 
                 if (input.equals("shut down")) 
                 {
                     shutDownServer();
                 }
-                
-                outputStream.writeUTF(input);
             }
         }
         catch (IOException e)
