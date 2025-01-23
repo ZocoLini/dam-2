f42efeb4a08f8931d1bcc3d080e73e08f6e6e872
Borja Castellano <borjacastellano1@gmail.com>
2025-01-23 12:33:41 +0100

    (sxe) añadiendo fieds a un modelo de odoo

--- sxe/ud4/my_hostel_profe/models/hostel.py
+++ sxe/ud4/my_hostel_profe/models/hostel.py
@@ -9,3 +9,8 @@ class Hostel(models.Model):
     Codigo = fields.Char(string="Codigo", required=True)
     Direccion1 = fields.Char("Direccion1")
     Direccion2 = fields.Char("Direccion2")
+
+    dias = fields.Integer(string="Dias", defaault=0)
+    abierto = fields.Boolean(string="Abierto", readonly=True)
+    estado = fields.Selection([{'0', 'Bueno'}, {'1', 'Regular'}, {'2', 'Malo'}], string="Estado", default='0')
+    propietario = fields.Many2one('hr.employee', string="Propietario")
