2de999c34c8ecc463d66241a4b5f659365533a6e
Borja Castellano <borjacastellano1@gmail.com>
2025-01-29 11:01:58 +0100

    (sxe) added fields

--- sxe/ud4/instituto/models/curso.py
+++ sxe/ud4/instituto/models/curso.py
@@ -9,6 +9,9 @@ class Curso(models.Model):
     description = fields.Text(string = 'Descripción')
     active = fields.Boolean(string='Active')
 
+    nombre_profesor = fields.Text(string='Nombre del profesor')
+    numero_horas = fields.Integer(string='Número de horas')
+
     def _compute_urgente(self):
         for record in self:
             if record.dias > 7:
