7b8d72e4c1cfb91124bb76f2f0dc0f63e80ee012
Borja Castellano <borjacastellano1@gmail.com>
2025-02-15 11:16:22 +0100

    (sxe) trt module 17.0.1.0.1

--- sxe/ud4/trt/__manifest__.py
+++ sxe/ud4/trt/__manifest__.py
@@ -2,7 +2,7 @@
     "name": "The Round Table Software Managment",
     "summary": "Manage users, plugins and licenses related to TRT",
     "description": "Module to manage users, plugins and licenses related to The Round Table software",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "author": "LebaStudios Software Solutions",
     "category": "Tools",
     "website": "https://www.lebastudios.org",
