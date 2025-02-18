b0b58a9187dbc086c9102ccb4f266f3ac1ca6ad6
Borja Castellano <borjacastellano1@gmail.com>
2025-02-18 21:44:39 +0100

    (sxe) adding plugin verification + compute

--- sxe/ud4/trt/models/plugin.py
+++ sxe/ud4/trt/models/plugin.py
@@ -8,19 +8,19 @@ class Plugin(models.Model):
 
     name = fields.Char(string="Name", required=True)
     description = fields.Text(string="Description")
-    is_monthly = fields.Boolean(string="Monthly", readonly=True, compute='_compute_is_monthly', store=True)
+    is_monthly = fields.Boolean(string="Monthly", readonly=True)
     price = fields.Float(string="Price", required=True)
     repository = fields.Char(string="Repository")
 
     licenses = fields.Many2many('trt.license', string="Licenses")
 
-    @api.depends('price')
-    def _compute_is_monthly(self):
-        for record in self:
-            record.is_monthly = record.price > 0
-
-    @api.constraints('price')
-    def _check_min_price(self):
-        for record in self:
-            if record.price < 0:
-                raise ValidationError("The price must be equal or greater than 0")
+    # @api.depends('price')
+    # def _compute_is_monthly(self):
+    #     for record in self:
+    #         record.is_monthly = record.price > 0
+#
+    # @api.constraints('price')
+    # def _check_min_price(self):
+    #     for record in self:
+    #         if record.price < 0:
+    #             raise ValidationError("The price must be equal or greater than 0")
