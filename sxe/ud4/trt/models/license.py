2ab66e92d4309157fa832c02341fd4548255674c
Borja Castellano <borjacastellano1@gmail.com>
2025-02-18 19:08:47 +0100

    (sxe) trying to generate default license if not set

--- sxe/ud4/trt/models/license.py
+++ sxe/ud4/trt/models/license.py
@@ -6,10 +6,10 @@ class License(models.Model):
     _description = "Users Licenses and the plugins purchased"
 
     name = fields.Char(string="Number", required=True)
-    # Monthly: Default type, pays monthly based on plugins price
+    # Default: Default type, pays monthly based on plugins price
     # Life-Time: Pays once the license and can use any plugin for life
     # Student: Special type for students, free to use any plugin but requires verification and renewal every year
-    type = fields.Selection([{'0', 'Monthly'}, {'1', 'LifeTime'}, {'2', 'Student'}], string="Type", default='0', required=True)
+    type = fields.Selection([{'0', 'Default'}, {'1', 'LifeTime'}, {'2', 'Student'}], string="Type", default='0', required=True)
 
     user = fields.Many2one('trt.user', string="User", required=True)
     plugins = fields.Many2many('trt.plugin', string="Plugins")
--- sxe/ud4/trt/models/user.py
+++ sxe/ud4/trt/models/user.py
@@ -1,4 +1,11 @@
+import random
+import string
+
 from odoo import fields, models, api
+
+def _generate_license_number():
+    random_part = ''.join(random.choices(string.ascii_uppercase + string.digits, k=8))
+    return f"LIC-{random_part}"
 
 
 class License(models.Model):
@@ -8,7 +15,19 @@ class License(models.Model):
     name = fields.Char(string="Email", required=True)
     password = fields.Char(string="Password", required=True)
 
-    licenses = fields.One2many('trt.license', 'user', string="Licenses")
+    licenses = fields.One2many('trt.license', 'user', string="Licenses", default=lambda self: self._get_default_license())
+
+    @api.model
+    def _get_default_license(self):
+        license_number = _generate_license_number()
+
+        license_vals = {
+            'name': license_number,
+            'type': '0',
+            'validity_date': fields.Date.today(),
+        }
+
+        return [(0, 0, license_vals)]
 
     # @api.depends('dias')
     # def _compute_urgente(self):
@@ -18,8 +37,8 @@ class License(models.Model):
     #         else:
     #             record.abierto = False
 
-    # @api.constraints('dias')
-    # def _check_dias(self):
+    # @api.constraints('licenses')
+    # def _check_min_licenses(self):
     #     for record in self:
     #         if record.alias > 7:
     #             raise ValidationError("Validacion para el campo")
