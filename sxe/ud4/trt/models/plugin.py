875f39da72c922bdc44cd13e1713e7fbd3468a8d
Borja Castellano <borjacastellano1@gmail.com>
2025-02-18 20:58:36 +0100

    (sxe) adding a constraint to the plugin price to not be negative

--- sxe/ud4/trt/models/plugin.py
+++ sxe/ud4/trt/models/plugin.py
@@ -13,3 +13,9 @@ class Plugin(models.Model):
     repository = fields.Char(string="Repository")
 
     licenses = fields.Many2many('trt.license', string="Licenses")
+
+    @api.constraints('price')
+    def _check_min_price(self):
+        for record in self:
+            if record.price < 0:
+                raise ValidationError("The price must be equal or greater than o")
